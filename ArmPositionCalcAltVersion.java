542441a4bb0b6b213ac06536858c7b077c0f7a1a
redPlover <100500945+redPlover@users.noreply.github.com>
2023-03-07 17:01:14 -0500

    Added more logic

--- ArmPositionCalcAltVersion.java
+++ ArmPositionCalcAltVersion.java
@@ -1,26 +1,31 @@
 // Note: This is an alternative version of the primary file
+// Every angle is in radians
 
 public class ArmPositionCalcAltVersion {
-    private static final double maxHeight = 10; // The maximum height of the arm
-    private static final double maxReach = 10; // The maximum reach of the arm
+    private static final double length; // The length of the first and second segments
+    private static final double maxReach; // The maximum reach of the arm
+    private static final double maxRotation; // The maximum rotation for every pivot
 
-    public ArmPositionCalcAltVersion() {}
+    public ArmPositionCalcAltVersion(double l, double rot) {
+        length = l;
+
+        maxReach = 2*length;
+        maxRotation = rot;
+    }
     /** 
      * Calculates the position of each of the arm segments required to reach the position of the given setpoint.
      * 
      * @param x the x coordinate (distance away) of the point
      * @param y the y coordinate of the point
      * @param z the z coordinate (height) of the point
+     * @param pitch the desired pitch of the final pivot section in radians
+     * @param yaw the desired pitch of the final pivot section in radians
      * 
-     * @return The position of each of the arm segments in the form of an array of doubles.
-     * The array goes as follows: [Base Rotation, First Pivot, Second Pivot, Third Pivot]
+     * @return The rotation of each of the arm segments in the form of an array of doubles, all in radians.
+     * The array goes as follows: [Base Rotation, First Pivot, Second Pivot, Third Pivot, Third Cross Pivot]
      */
-    public static double[] findPositions(double x, double y, double z) {
+    public static double[] findPositions(double x, double y, double z, double pitch, double yaw) {
         double[] result = new double[4];
-
-        // Checks if the point is within the reach of the arm
-        if(z <= 0 || z > maxHeight || Math.sqrt( + Math.sqrt(x*x + y*y)*Math.sqrt(x*x + y*y)) > maxReach)
-            return null;
 
         // Calculates the base rotation
         if(x == 0)
@@ -30,7 +35,16 @@ public class ArmPositionCalcAltVersion {
         result[0] = Math.atan2(y, x);
 
         // Converts the x into a value relative to the base (using Pythagorean Theorem)
-        double b = Math.sqrt(x*x + y*y);
+        // and y to the vertical axis.
+        // This is for ease of understanding the later code.
+        double x = Math.sqrt(x*x + y*y);
+        double y = z;
+
+        // Checks if the point is within the reach of the arm
+        if(z <= 0 || Math.sqrt(x*x + y*y) > maxReach || x < 0)
+            return null;
+
+        
 
         return result;
     }
